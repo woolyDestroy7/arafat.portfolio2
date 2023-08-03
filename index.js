90bb0c809a2b215c885caf4f0a6621a0d0a27a5e
woolyDestroy7 <120059280+woolyDestroy7@users.noreply.github.com>
2023-08-03 14:26:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+// ---
+let count = document.querySelectorAll(".count")
+let arr = Array.from(count)
+
+
+
+arr.map(function(item){
+  let startnumber = 0
+
+  function counterup(){
+  startnumber++
+  item.innerHTML= startnumber
+   
+  if(startnumber == item.dataset.number){
+      clearInterval(stop)
+  }
+}
+
+let stop =setInterval(function(){
+  counterup()
+},50)
+
+})
+// ---
+
+ // Function to handle form submission
+ function handleSubmit(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  
+  // Get the values from the form fields
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  const message2 = `Hello Arafat
+I'm ${name},
+and i want to tell you that:
+"${message}"
+,And this is my E-mail:${email}`;
+
+  const url = `https://api.whatsapp.com/send?phone=+201060929210&text=${encodeURIComponent(message2)}`;
+  // Redirect to the URL
+  window.location.href = url;
+}
+
+// Add the event listener to the form
+const form = document.getElementById("myForm");
+form.addEventListener("submit", handleSubmit);
+
+
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
